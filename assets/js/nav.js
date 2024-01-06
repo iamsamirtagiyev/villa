94b7981b80d9f656554d4bad22a29e9dc3ed3a7d
iamsamirtagiyev <stagiyev579@gmail.com>
2024-01-06 11:13:50 +0400

    villa

--- assets/js/nav.js
+++
@@ -1,7 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon')
-const menu = document.querySelector('.menu')
-
-menuIcon.onclick = () => {
-    menu.classList.toggle('active')
-    menuIcon.classList.toggle('active')
-}
